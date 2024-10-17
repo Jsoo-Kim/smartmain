1d1d426b689bfca085a35fd293bd2ef3db6c746a
Jisoo Kim <ooSoo2Soo2oo@gmail.com>
2024-10-17 23:26:23 +0900

    first

---
+++ index.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const http = require("http");
+
+const express = require("express");
+const app = express();
+const server = http.createServer(app);
+
+// Express.js와 같은 Node.js 서버를 사용할 때, 정적 파일(CSS, JS, 이미지 등)을 올바르게 제공하기 위해 express.static 미들웨어를 사용
+// 이 미들웨어를 통해 특정 디렉토리를 "정적 파일"로 서빙하도록 설정할 수 있음
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/smart_main", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/html/main.html"));
+});
+
+app.get("/smart_chatbot", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/html/chatbot.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
